7078aa929ea466ae02760e438cc8fa15422474ce
LAPTOP-E4RCDK1Q\Camilla <kamilladzuldybaeva@gmail.com>
2023-11-07 22:48:13 +0500

    Add some more tests for singleshot_apptimer

--- tests/singleshot_apptimer_tests.cpp
+++ tests/singleshot_apptimer_tests.cpp
@@ -12,7 +12,7 @@
 #include "simpletest/simpletest.hpp"
 
 
-DEFINE_TEST(AsyncTimer_Simple)
+DEFINE_TEST(AsyncTimer_Simple1)
 {
     static volatile bool called;
     called = false;
@@ -29,4 +29,92 @@ DEFINE_TEST(AsyncTimer_Simple)
 
     nrf_delay_ms(400);
     TEST(called);
+}
+
+
+DEFINE_TEST(AsyncTimer_Simple2)
+{
+    static volatile bool called;
+    called = false;
+
+    nrf::singleshot_apptimer tt;
+    tt.async_wait(
+        300,
+        [](error::error_status e)
+        {
+            if (!e)
+                called = true;
+        }
+    );
+
+    nrf_delay_ms(200);
+    TEST(!called);
+    nrf_delay_ms(200);
+    TEST(called);
+}
+
+
+DEFINE_TEST(AsyncTimer_Simple3)
+{
+    static volatile bool called;
+    called = false;
+
+    nrf::singleshot_apptimer tt;
+    tt.async_wait(
+        300,
+        [](error::error_status e)
+        {
+            if (!e)
+                called = true;
+        }
+    );
+
+    tt.cancel();
+    TEST(!called);
+}
+
+
+DEFINE_TEST(AsyncTimer_Simple4)
+{
+    static volatile bool called;
+
+    nrf::singleshot_apptimer tt;
+    for (int i = 0; i < 3; i++)
+    {
+        called = false;
+        tt.async_wait(
+            300,
+            [](error::error_status e)
+            {
+                if (!e)
+                    called = true;
+            }
+        );
+
+        nrf_delay_ms(400);
+    }
+
+    TEST(called);
+}
+
+
+DEFINE_TEST(AsyncTimer_Simple5)
+{
+    static volatile bool called;
+    called = false;
+
+    nrf::singleshot_apptimer tt;
+    for (int i = 0; i < 3; i++)
+    {
+        tt.async_wait(
+            300,
+            [](error::error_status e)
+            {
+                if (!e)
+                    called = true;
+            }
+        );
+    }
+
+    TEST(!called);
 }
