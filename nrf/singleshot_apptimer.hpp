c52ee3d79d9bb6f19e13dceef5d45036cd815b64
LAPTOP-E4RCDK1Q\Camilla <kamilladzuldybaeva@gmail.com>
2023-11-24 00:09:40 +0500

    Move init and create of apptimer to constructor

--- nrf/singleshot_apptimer.hpp
+++ nrf/singleshot_apptimer.hpp
@@ -20,49 +20,41 @@ namespace nrf
 			: m_app_timer{0}
 			, m_p_app_timer{&m_app_timer}
 			, m_handler{}
-		{}
+		{
+            singleton_apptimer::init();
+            app_timer_create(&m_p_app_timer, APP_TIMER_MODE_SINGLE_SHOT, static_handler);
+        }
 
 	public: 
 
 		void async_wait(uint32_t time_ms, utils::static_function<void(error::error_status)> handler)
 		{
-		    singleton_apptimer::init();
-
-			error::error_status err = app_timer_create(&m_p_app_timer, APP_TIMER_MODE_SINGLE_SHOT, static_handler);
-
-			if (err)
-			{ 
-				handler(err);
-			}
-			else
-			{ 
-				err = app_timer_start(m_p_app_timer, APP_TIMER_TICKS(time_ms), this);
-				if (err)
-				    handler(err);
-				else
-					m_handler = handler;
-			}
+            error::error_status err = app_timer_start(m_p_app_timer, APP_TIMER_TICKS(time_ms), this);
+            if (err)
+                handler(err);
+            else
+                m_handler = handler;
 		}
 	   
-	    error::error_status cancel()
-		{
-			error::error_status err = app_timer_stop(m_p_app_timer);
-			return err;
-		}
+        error::error_status cancel()
+        {
+            error::error_status err = app_timer_stop(m_p_app_timer);
+            return err;
+        }
+
+    private:
+
+        static void static_handler(void* p_context)
+        {
+            singleshot_apptimer* ss = (singleshot_apptimer*)(p_context);
+            (ss->m_handler)(error::error_status());
+        }
 	    
 	private:
 
 		app_timer_t                                       m_app_timer;
 		app_timer_t*                                      m_p_app_timer;
 		utils::static_function<void(error::error_status)> m_handler;
-
-    private:
-
-		static void static_handler(void* p_context)
-        {
-			singleshot_apptimer* ss = (singleshot_apptimer*)(p_context);
-			(ss->m_handler)(error::error_status());
-		}
 	};
 }
 
