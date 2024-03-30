65d0ca8f080693d42e9f96e7511e973fd4af840e
LAPTOP-E4RCDK1Q\Camilla <kamilladzuldybaeva@gmail.com>
2024-03-30 23:42:08 +0500

    Add two new characteristics

--- estc_gatt_server/estc_service.h
+++ estc_gatt_server/estc_service.h
@@ -1,33 +1,3 @@
-/**
- * Copyright 2022 Evgeniy Morozov
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted provided that the following conditions are met:
- *
- * 1. Redistributions of source code must retain the above copyright notice,
- * this list of conditions and the following disclaimer.
- *
- * 2. Redistributions in binary form must reproduce the above copyright notice,
- * this list of conditions and the following disclaimer in the documentation
- * and/or other materials provided with the distribution.
- *
- * 3. Neither the name of the copyright holder nor the names of its contributors
- * may be used to endorse or promote products derived from this software without
- * specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
- * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY
- * WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE
-*/
-
 #ifndef ESTC_SERVICE_H__
 #define ESTC_SERVICE_H__
 
@@ -44,6 +14,8 @@
 
 // TODO: 3. Pick a characteristic UUID and define it:
 #define ESTC_GATT_CHAR_1_UUID 0xAB88
+#define ESTC_GATT_CHAR_NOTIFY_UUID 0x65DK
+#define ESTC_GATT_CHAR_INDICATE_UUID 0x0402
 
 typedef struct
 {
@@ -52,12 +24,13 @@ typedef struct
 
     // TODO: 6.3. Add handles for characterstic (type: ble_gatts_char_handles_t)
     ble_gatts_char_handles_t characteristic_handle;
+    ble_gatts_char_handles_t characteristic_notify_handle;
+    ble_gatts_char_handles_t characteristic_indicate_handle;
 } ble_estc_service_t;
 
 ret_code_t estc_ble_service_init(ble_estc_service_t *service);
 
-void estc_ble_service_on_ble_event(const ble_evt_t *ble_evt, void *ctx);
-
-void estc_update_characteristic_1_value(ble_estc_service_t *service, int32_t *value);
+void estc_update_characteristic_notify_value(ble_estc_service_t *service, uint8_t value);
+void estc_update_characteristic_indicate_value(ble_estc_service_t* service, uint8_t value);
 
 #endif /* ESTC_SERVICE_H__ */
