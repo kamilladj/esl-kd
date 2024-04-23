8325de02985f78cc9bf28bced535c394eef3bf5b
LAPTOP-E4RCDK1Q\Camilla <kamilladzuldybaeva@gmail.com>
2024-04-23 18:19:35 +0500

    Change serializer

--- nrf/flash/serializer.hpp
+++ nrf/flash/serializer.hpp
@@ -22,59 +22,59 @@ namespace nrf
     template<size_t size>
     void serialize(static_vector<uint8_t, size>& buff, uint16_t val)
     {
-        buff.push_back(val & 0xFF);
         buff.push_back((val >> 8) & 0xFF);
+        buff.push_back(val & 0xFF);
     }
 
     template<size_t size>
     void serialize(static_vector<uint8_t, size>& buff, int16_t val)
     {
-        buff.push_back(val & 0xFF);
         buff.push_back((val >> 8) & 0xFF);
+        buff.push_back(val & 0xFF);
     }
 
     template<size_t size>
     void serialize(static_vector<uint8_t, size>& buff, uint32_t val)
     {
-        buff.push_back(val & 0xFF);
-        buff.push_back((val >> 8) & 0xFF);
-        buff.push_back((val >> 16) & 0xFF);
         buff.push_back((val >> 24) & 0xFF);
+        buff.push_back((val >> 16) & 0xFF);
+        buff.push_back((val >> 8) & 0xFF);
+        buff.push_back(val & 0xFF);
     }
 
     template<size_t size>
     void serialize(static_vector<uint8_t, size>& buff, int32_t val)
     {
-        buff.push_back(val & 0xFF);
-        buff.push_back((val >> 8) & 0xFF);
-        buff.push_back((val >> 16) & 0xFF);
         buff.push_back((val >> 24) & 0xFF);
+        buff.push_back((val >> 16) & 0xFF);
+        buff.push_back((val >> 8) & 0xFF);
+        buff.push_back(val & 0xFF);
     }
 
     template<size_t size>
     void serialize(static_vector<uint8_t, size>& buff, uint64_t val)
     {
-        buff.push_back(val & 0xFF);
-        buff.push_back((val >> 8) & 0xFF);
-        buff.push_back((val >> 16) & 0xFF);
-        buff.push_back((val >> 24) & 0xFF);
-        buff.push_back((val >> 32) & 0xFF);
-        buff.push_back((val >> 40) & 0xFF);
-        buff.push_back((val >> 48) & 0xFF);
         buff.push_back((val >> 56) & 0xFF);
+        buff.push_back((val >> 48) & 0xFF);
+        buff.push_back((val >> 40) & 0xFF);
+        buff.push_back((val >> 32) & 0xFF);
+        buff.push_back((val >> 24) & 0xFF);
+        buff.push_back((val >> 16) & 0xFF);
+        buff.push_back((val >> 8) & 0xFF);
+        buff.push_back(val & 0xFF);
     }
 
     template<size_t size>
     void serialize(static_vector<uint8_t, size>& buff, int64_t val)
     {
-        buff.push_back(val & 0xFF);
-        buff.push_back((val >> 8) & 0xFF);
-        buff.push_back((val >> 16) & 0xFF);
-        buff.push_back((val >> 24) & 0xFF);
-        buff.push_back((val >> 32) & 0xFF);
-        buff.push_back((val >> 40) & 0xFF);
-        buff.push_back((val >> 48) & 0xFF);
         buff.push_back((val >> 56) & 0xFF);
+        buff.push_back((val >> 48) & 0xFF);
+        buff.push_back((val >> 40) & 0xFF);
+        buff.push_back((val >> 32) & 0xFF);
+        buff.push_back((val >> 24) & 0xFF);
+        buff.push_back((val >> 16) & 0xFF);
+        buff.push_back((val >> 8) & 0xFF);
+        buff.push_back(val & 0xFF);
     }
 
     template<size_t size>
@@ -86,36 +86,38 @@ namespace nrf
     /*template<size_t size>
     void serialize(static_vector<uint8_t, size>& buff, short int val)
     {
-        size_t res = 0;
-
-        for (size_t i = 0; i < sizeof(long long int); i++, res += 8)
-            buff.push_back((val >> res) & 0xFF);
+        buff.push_back((val >> 8) & 0xFF);
+        buff.push_back(val & 0xFF);
     }*/
 
     template<size_t size>
     void serialize(static_vector<uint8_t, size>& buff, int val)
     {
-        size_t res = 0;
-
-        for (size_t i = 0; i < sizeof(long long int); i++, res += 8)
-            buff.push_back((val >> res) & 0xFF);
+        buff.push_back((val >> 24) & 0xFF);
+        buff.push_back((val >> 16) & 0xFF);
+        buff.push_back((val >> 8) & 0xFF);
+        buff.push_back(val & 0xFF);
     }
 
     /*template<size_t size>
     void serialize(static_vector<uint8_t, size>& buff, long int val)
     {
-        size_t res = 0;
-
-        for (size_t i = 0; i < sizeof(long long int); i++, res += 8)
-            buff.push_back((val >> res) & 0xFF);
+        buff.push_back((val >> 24) & 0xFF);
+        buff.push_back((val >> 16) & 0xFF);
+        buff.push_back((val >> 8) & 0xFF);
+        buff.push_back(val & 0xFF);
     }
 
     template<size_t size>
     void serialize(static_vector<uint8_t, size>& buff, long long int val)
     {
-        size_t res = 0;
-
-        for (size_t i = 0; i < sizeof(long long int); i++, res += 8)
-            buff.push_back((val >> res) & 0xFF);
+        buff.push_back((val >> 56) & 0xFF);
+        buff.push_back((val >> 48) & 0xFF);
+        buff.push_back((val >> 40) & 0xFF);
+        buff.push_back((val >> 32) & 0xFF);
+        buff.push_back((val >> 24) & 0xFF);
+        buff.push_back((val >> 16) & 0xFF);
+        buff.push_back((val >> 8) & 0xFF);
+        buff.push_back(val & 0xFF);
     }*/
 }
