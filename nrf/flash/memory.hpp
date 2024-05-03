d0c540e80180ef7cb534e534476ce455321f447f
LAPTOP-E4RCDK1Q\Camilla <kamilladzuldybaeva@gmail.com>
2024-05-03 18:05:35 +0500

    Add functions to find necessary page by checking page headers

--- nrf/flash/memory.hpp
+++ nrf/flash/memory.hpp
@@ -17,6 +17,7 @@
 namespace nrf
 {
     template<size_t size>
+    template<size_t record_size, size_t header_size = 8>
     class memory
     {
     public:
@@ -24,63 +25,113 @@ namespace nrf
         memory()
             : m_start_addr{ BOOTLOADER_ADDRESS - NRF_DFU_APP_DATA_AREA_SIZE }
             , m_end_addr{ BOOTLOADER_ADDRESS - 1}
-            , m_pages{ page<m_page_header_size, size>(m_start_addr, m_start_addr + CODE_PAGE_SIZE - 1)
-                     , page<m_page_header_size, size>(m_start_addr + CODE_PAGE_SIZE, m_start_addr + 2 * CODE_PAGE_SIZE - 1)
-                     , page<m_page_header_size, size>(m_start_addr + 2 * CODE_PAGE_SIZE, m_end_addr) }
+            , m_pages{ page<record_size, header_size>(m_start_addr, m_start_addr + CODE_PAGE_SIZE - 1)
+                     , page<record_size, header_size>(m_start_addr + CODE_PAGE_SIZE, m_start_addr + 2 * CODE_PAGE_SIZE - 1)
+                     , page<record_size, header_size>(m_start_addr + 2 * CODE_PAGE_SIZE, m_end_addr) }  
             , m_cur_page{ 0 }
         {
             NRFX_ASSERT(m_num_of_pages >= 2);
-            //write_page_headers();
+            check_pages_format();
+            find_cur_page();
+        }
+        
+    private:
+
+        void check_pages_format()
+        {
+            for (size_t i = 0; i < m_num_of_pages; i++)
+            {
+                if (!is_page_header_valid(i))
+                    edit_page(i, 0);
+
+                m_pages[i].find_cur_pos();
+            }
+
+            NRFX_ASSERT(are_all_page_headers_valid());
+        }
+
+        void find_cur_page()
+        {
+            size_t max = m_pages[0].get_index(m_page_index_offset);
+
+            for (size_t i = 0; i < m_num_of_pages; i++)
+            {
+                size_t index = m_pages[i].get_index(m_page_index_offset);
+
+                if (index > max)
+                {
+                    max = index;
+                    m_cur_page = i;
+                }
+
+                if (index == 100 && m_pages[i].is_full())
+                {
+                    m_cur_page++;
+                    m_cur_page %= m_num_of_pages;
+                    return;
+                }
+            }
+        }
+
+        void edit_page(size_t i, size_t index)
+        {
+            m_pages[i].erase();
+
+            static_vector<uint8_t, header_size> buff;
+            fill_page_header(index, buff);
+            m_pages[i].write_page_header(buff);
+        }
+
+        void switch_to_next_page()
+        {
+            size_t index = m_pages[m_cur_page].get_index(m_page_index_offset);
+
+            m_cur_page++;
+            m_cur_page %= m_num_of_pages;
+
+            size_t new_index = index++;
+            if (new_index > 100)
+                new_index = 0;
+
+            edit_page(m_cur_page, new_index);
         }
 
     private:
 
-        //bool validate_page_header()
-        //{
-        //    read_page_header(i);
+        bool is_page_header_valid(size_t i)
+        {
+            static_vector<uint8_t, header_size> buff;
 
-        //    for (size_t i = 0; i < m_num_of_pages; i++)
-        //        read_page_headers(i, 0);
-        //}
+            m_pages[i].read_page_header(buff);
 
-        //void write_page_headers()
-        //{
-        //    for (size_t i = 0; i < m_num_of_pages; i++)
-        //        write_page_header(i, 0);
-        //}
+        }
 
-        //void write_page_header(size_t i, uint8_t idx)
-        //{
-        //    static_vector<uint8_t, m_page_header_size> vec;
+        bool are_all_page_headers_valid()
+        {
+            for (size_t i = 0; i < m_num_of_pages; i++)
+            {
+                if (!is_page_header_valid(i))
+                    return false;
+            }
 
-        //    vec.push_back(0);
-        //    vec.push_back(255);
-        //    vec.push_back(250); //65530
-        //    vec.push_back(1);
-        //    vec.push_back(119); // version 375
-        //    vec.push_back(idx); //index
-        //    vec.push_back(0);
-        //    vec.push_back(7); //crc 111
-        //    vec.push_back(0);
-        //    vec.push_back(0); //reserved
+            return true;
+        }
 
-        //    m_pages[i].write_page_header(vec);
-
-        //    cnt++;
-        //}
-
-        //void rewrite_page_header()
-        //{
-        //    static uint8_t cnt = 1;
-
-        //    write_page_header(m_cur_page, cnt);
-
-        //    cnt++;
-        //}
+        void fill_page_header(uint8_t index, static_vector<uint8_t, header_size>& buff)
+        {
+            buff.push_back(255);
+            buff.push_back(250); //id 65530
+            buff.push_back(1);
+            buff.push_back(119); //version 375
+            buff.push_back(index); //index
+            buff.push_back(26);
+            buff.push_back(187); //crc 6843
+            buff.push_back(0); //reserved
+        }
 
     private:
 
-        bool is_record_writable(const static_vector<uint8_t, size>& buff)
+        bool is_record_writable(const static_vector<uint8_t, record_size>& buff)
         {
             if (buff[0] == 0xFF)
                 return false;
@@ -90,9 +141,9 @@ namespace nrf
 
     public:
 
-        bool read_from_page(static_vector<uint8_t, size>& buff)
+        bool read_record_from_page(static_vector<uint8_t, record_size>& buff)
         {
-            if (m_pages[m_cur_page].is_page_empty())
+            if (m_pages[m_cur_page].is_empty())
                 return false;
             else
                 m_pages[m_cur_page].read_last_record(buff);
@@ -100,7 +151,7 @@ namespace nrf
             return true;
         }
 
-        void write_to_page(const static_vector<uint8_t, size>& buff)
+        void write_record_to_page(const static_vector<uint8_t, record_size>& buff)
         {
             if (!(is_record_writable(buff)))
             {
@@ -108,30 +159,29 @@ namespace nrf
                 return;
             }
 
-            if (m_pages[m_cur_page].is_page_full())
+            if (m_pages[m_cur_page].is_full())
             {
-                m_cur_page++;
-                m_cur_page %= m_num_of_pages;
-                m_pages[m_cur_page].page_erase();
-                //rewrite_page_header();
+                switch_to_next_page();
+                m_pages[m_cur_page].find_cur_pos();
             }
 
             m_pages[m_cur_page].write_new_record(buff);
         }
-        
+
     private:
 
         uint32_t                       m_start_addr;
         uint32_t                       m_end_addr;
-        static const uint32_t          m_page_header_size = 10;
+        static const uint32_t          m_page_index_offset = 4;
         static const uint32_t          m_num_of_pages = NRF_DFU_APP_DATA_AREA_SIZE / CODE_PAGE_SIZE;
-        page<m_page_header_size, size> m_pages[m_num_of_pages];
+        page<record_size, header_size> m_pages[m_num_of_pages];
         uint8_t                        m_cur_page;
     };
 
-    template<size_t size>
-    const uint32_t memory<size>::m_page_header_size;
+    template<size_t record_size, size_t header_size>
+    const uint32_t memory<record_size, header_size>::m_page_index_offset;
 
-    template<size_t size>
-    const uint32_t memory<size>::m_num_of_pages;
+    template<size_t record_size, size_t header_size>
+    const uint32_t memory<record_size, header_size>::m_num_of_pages;
+
 }
