aaa4f31d5c75550e0233f87fb7f72cf8d6df4104
LAPTOP-E4RCDK1Q\Camilla <kamilladzuldybaeva@gmail.com>
2024-05-03 18:07:25 +0500

    Add crc calculation

--- nrf/flash/memory.hpp
+++ nrf/flash/memory.hpp
@@ -16,7 +16,8 @@
 
 namespace nrf
 {
-    template<size_t size>
+    uint16_t crc16(const uint8_t* data_p, uint8_t length);
+
     template<size_t record_size, size_t header_size = 8>
     class memory
     {
@@ -184,4 +185,19 @@ namespace nrf
     template<size_t record_size, size_t header_size>
     const uint32_t memory<record_size, header_size>::m_num_of_pages;
 
+
+    //from https://stackoverflow.com/questions/10564491/function-to-calculate-a-crc16-checksum
+    uint16_t crc16(const uint8_t* data_p, uint8_t length)
+    {
+        uint8_t x;
+        uint16_t crc = 0xFFFF;
+
+        while (length--) {
+            x = crc >> 8 ^ *data_p++;
+            x ^= x >> 4;
+            crc = (crc << 8) ^ ((uint16_t)(x << 12)) ^ ((uint16_t)(x << 5)) ^ ((uint16_t)x);
+        }
+
+        return crc;
+    }
 }
